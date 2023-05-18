ae277878cf8a645ef372be0c953ed153955d23f9
Artem Tanyhin <magic.cat.775@gmail.com>
2023-05-18 15:22:20 -0400

    fix: mocks

--- server/src/index.js
+++ server/src/index.js
@@ -1,7 +1,7 @@
 const { ApolloServer } = require("@apollo/server");
 const { startStandaloneServer } = require("@apollo/server/standalone");
 
-const { addMockToSchema } = require("@graphql-tools/mock");
+const { addMocksToSchema } = require("@graphql-tools/mock");
 const { makeExecutableSchema } = require("@graphql-tools/schema");
 
 const typeDefs = require("./schema");
@@ -10,7 +10,7 @@ const mocks = {
     Query: () => ({
         tracksForHome: () => [...new Array(6)],
     }),
-    
+
     Track: () => ({
         id: () => "track_01",
         title: () => "Astro Kitty, Space Explorer",
@@ -30,10 +30,10 @@ const mocks = {
 
 async function startApolloServer() {
     const server = new ApolloServer({ 
-        schema: addMockToSchema({
+        schema: addMocksToSchema({
             schema: makeExecutableSchema({ typeDefs }),
+            mocks
         }),
-        mocks
      });
     const { url } = await startStandaloneServer(server);
     console.log(`Server is running at: ${url}`);
